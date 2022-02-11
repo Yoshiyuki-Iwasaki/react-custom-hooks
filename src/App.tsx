a0a74bfce6262650f385f9e3d22c75063883c4d6
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-02-11 22:37:30 +0900

    fix component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Top from "./page/top";
 import Post from "./page/post";
-import User from "./page/user";
+import Couter from "./page/counter";
 import Layout from "./Layout";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
@@ -9,9 +9,18 @@ function App() {
     <>
       <Layout>
         <Router>
+          <h1>React Custom Hooks</h1>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/couter">Couter</a>
+            </li>
+          </ul>
           <Routes>
             <Route path={"/"} element={<Top />} />
-            <Route path={"/user"} element={<User />} />
+            <Route path={"/couter"} element={<Couter />} />
             <Route path={"/post"} element={<Post />} />
           </Routes>
         </Router>
